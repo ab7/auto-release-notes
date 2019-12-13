03d35812f0bef5013b0662f95eb63acbef1bd67d
Alex Baker <hello@alexbaker.io>
2019-12-13 10:36:45 -0700

    Refactor Github webhook validation

--- main.py
+++ main.py
@@ -3,27 +3,43 @@ import hmac
 import hashlib
 
 
-GITHUB_SECRET = os.environ.get('GITHUB_SECRET', '')
+GITHUB_SECRET = os.environ.get('GITHUB_SECRET')
+
+
+class GithubRequestException(Exception):
+    pass
+
+
+class GithubRequestValidator:
+
+    def __init__(self, secret):
+        self.secret = secret
+
+    @staticmethod
+    def _check_method(method):
+        if method != 'POST':
+            message = f'Method not allowed: {method}'
+            raise GithubRequestException(message)
+
+    def _check_signature(self, signature, raw_data):
+        digest_maker = hmac.new(bytes(self.secret, 'utf-8'), raw_data, hashlib.sha1)
+        if signature.split('=')[1] != digest_maker.hexdigest():
+            message = f'Invalid signature: {signature}'
+            raise GithubRequestException(message)
+
+    def validate_webhook(self, request):
+        method = request.method
+        signature = request.headers.get('X-Hub-Signature', '')
+        raw_data = request.get_data()
+
+        self._check_method(method)
+        self._check_signature(signature, raw_data)
 
 
 def update_release_notes(request):
-    if request.method != 'POST':
-        print(f'Method not allowed: {request.method}')
-        return '', 302
-
     try:
-        signature = request.headers['X-Hub-Signature']
-    except KeyError:
-        print('Missing X-Hub-Signature header')
-        return '', 302
-
-    digest_maker = hmac.new(
-        bytes(GITHUB_SECRET, 'utf-8'),
-        request.get_data(),
-        hashlib.sha1
-    )
-    if signature.split('=')[1] != digest_maker.hexdigest():
-        print(f'Invalid signature: {signature}')
+        GithubRequestValidator(GITHUB_SECRET).validate_webhook(request)
+    except GithubRequestException:
         return '', 302
 
     print('success!')
